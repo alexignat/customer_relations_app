49fd5a7ece42f7cda50ff8e90af2c3667db269bb
alexignat <d.alexandru.ignat@gmail.com>
2015-06-13 12:42:03 -0400

    Created contact class

--- contact.rb
+++ contact.rb
@@ -1,64 +1,8 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# # This is the file for the contact class
-
-# class Contact
-#   def initialize(first_name, last_name, email, notes)
-#     @first_name = first_name.capitalize
-#     @last_name = last_name.capitalize
-#     @email = email
-#     @notes = notes
-#   end
-
-#   def to_s
-#     puts "Contact name: #{@first_name} #{@last_name}\nEmail: #{@email}\nNotes: #{@notes}"
-#   end
-# end
+class Contact
+  def initialize(first_name, last_name, email, notes)
+    @first_name = first_name
+    @last_name = last_name
+    @email = email
+    @ notes = notes
+end
 
