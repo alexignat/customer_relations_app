c41ccddc585361714498253f6847b582f1200059
alexignat <d.alexandru.ignat@gmail.com>
2015-06-13 12:06:40 -0400

    Restarted project

--- contact.rb
+++ contact.rb
@@ -1,15 +1,64 @@
-# This is the file for the contact class
 
-class Contact
-  def initialize(first_name, last_name, email, notes)
-    @first_name = first_name.capitalize
-    @last_name = last_name.capitalize
-    @email = email
-    @notes = notes
-  end
 
-  def to_s
-    puts "Contact name: #{@first_name} #{@last_name}\nEmail: #{@email}\nNotes: #{@notes}"
-  end
-end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # This is the file for the contact class
+
+# class Contact
+#   def initialize(first_name, last_name, email, notes)
+#     @first_name = first_name.capitalize
+#     @last_name = last_name.capitalize
+#     @email = email
+#     @notes = notes
+#   end
+
+#   def to_s
+#     puts "Contact name: #{@first_name} #{@last_name}\nEmail: #{@email}\nNotes: #{@notes}"
+#   end
+# end
 
--- crm.rb
+++ crm.rb
@@ -1,52 +1,118 @@
-# CRM app
 
-# This method prints out the main method
-class CRM
 
-  def initialize(name)
-    @name = name
-  end
 
-  def print_menu
-    puts "[1] Add a new contact"
-    puts "[2] Edit an existing contact"
-    puts "[3] Delete a contact"
-    puts "[4] Display all the contacts"
-    puts "[5] Display an attribute"
-    puts "[6] Exit"
-  end
 
-  # This method chooses a command
 
-  def main_menu
-    print_menu
-    get_command = gets.to_i
-    execute_command(get_command)
-  end
 
-  #This method executes commands
 
-  def execute_command
-    add_contact if execute_command == 1
-    edit_contact if execute_command == 2
-    delete_contact if execute_command == 3
-    display_contacts if execute_command == 4
-    display_attribute if execute_command == 5
-    exit_menu if execute_command == 6
-  end
 
-  def add_contact
-    print "Enter First Name: "
-    first_name = gets.chomp.capitalize
-    print "Enter Last Name: "
-    last_name = gets.chomp.capitalize
-    print "Enter Email Address: "
-    email = gets.chomp
-    print "Enter a Note: "
-    note = gets.chomp
-    contact = Contact.new(first_name, last_name, email, note)
-  end
-end
 
-friends = CRM.new("friends")
-friends.main_menu
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # CRM app
+
+
+
+# # This method prints out the main method
+# class CRM
+
+#   def initialize(name)
+#     @name = name
+#   end
+
+#   # This method prints out the main menu
+#   def print_main_menu
+#     puts "[1] Add a new contact"
+#     puts "[2] Edit an existing contact"
+#     puts "[3] Delete a contact"
+#     puts "[4] Display all the contacts"
+#     puts "[5] Display an attribute"
+#     puts "[6] Exit"
+#   end
+
+#   #This method chooses a command
+
+#   def choose_option(choice)
+#     case choice
+#     when 1 then add_contact
+#     when 2 then edit_contact
+#     when 3 then delete_contact
+#     when 4 then display_contact
+#     when 5 then display_attribute
+#     when 6 then exit
+#     else
+#       puts "I can't do that"
+#     end
+#   end
+
+#   # This method will take the command and execute it
+
+#   def main_menu
+#     while true
+#       print main_menu
+
+#       print "Select an option: "
+#       option = gets.chomp.to_i
+
+#       choose_option(choice)
+#     end
+#   end
+
+#   def add_contact
+#     print "Enter First Name: "
+#     first_name = gets.chomp.capitalize
+#     print "Enter Last Name: "
+#     last_name = gets.chomp.capitalize
+#     print "Enter Email Address: "
+#     email = gets.chomp
+#     print "Enter a Note: "
+#     note = gets.chomp
+#     contact = Contact.new(first_name, last_name, email, note)
+#   end
+# end
+
+# friends = CRM.new("friends")
+# friends.main_menu
